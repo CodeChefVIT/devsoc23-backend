4f22b2d45f3791f5acfa789569de56a034a2e8e4
sugamkuber <>
2023-02-28 00:02:41 +0530

    feat: Adding user models

---
+++ models/userModel.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	Id           primitive.ObjectID `bson:"_id"`
+	FirstName    *string            `json:"firstName" validate:"required,min=2,max=16"`
+	LastName     *string            `json:"lastName" validate:"min=2,max=32"`
+	Email        *string            `json:"email" validate:"required,email"`
+	Password     *string            `json:"password" validate:"required,min=8,max=64"`
+	PhoneNumber  *string            `json:"phoneNumber" validate:"required,e164,min=10,max=13"`
+	Token        *string            `json:"token,omitempty"`
+	UserRole     *string            `json:"userRole" default:"USER" validate:"eq=ADMIN|eq=HACKER|eq=VOTER"`
+	ProfilePhoto *string            `json:"profilePhoto,omitempty"`
+	College      *string            `json:"college,omitempty" validate:"min=10,max=128"`
+	CollegeYear  *string            `json:"collegeYear,omitempty"`
+	BirthDate    *string            `json:"birthData,omitempty"`
+	VerifyOtp    *string            `json:"verifyOtp,omitempty"`
+	Address      *string            `json:"address,omitempty"`
+	QrData       *string            `json:"qrData,omitempty"`
+	IsActive     bool               `json:"isActive,omitempty"`
+	IsVerify     bool               `json:"isVerify,omitempty"`
+	IsCanShare   bool               `json:"isCanShare,omitempty"`
+	IsCheckedIn  bool               `json:"isCheckedIn,omitempty"`
+	InTeam       bool               `json:"inTeam,omitempty"`
+	IsBoard      bool               `json:"isBoard,omitempty" default:"USER"`
+	CreatedAt    time.Time          `json:"createdTime"`
+	UpdatedAt    time.Time          `json:"updatedTime"`
+	TeamId       *string            `json:"teamId,omitempty"`
+	UserId       string             `json:"userId"`
+}
