84366afc0b17be4e2a52d23a5089912a330d57be
Akarsh Shukla <vyoopsoftwares@gmail.com>
2023-03-25 03:04:36 +0530

    project apis and routes

---
+++ models/projectModel.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Project struct {
+	Id                 primitive.ObjectID `bson:"_id"`
+	TeamId             *string            `bson:"teamId"`
+	ProjectName        *string            `json:"projectName"`
+	ProjectDescription *string            `json:"projectDescription"`
+	ProjectStatus      *string            `json:"projectStatus,omitempty"`
+	ProjectVideoLink   *string            `json:"projectVideoLink"`
+	ProjectGithubLink  *string            `json:"projectGithubLink"`
+	ProjectTrack       *string            `json:"projectTrack"`
+	ProjectTags        []string           `json:"projectTags"`
+	IsFinal            bool               `json:"isFinal,omitempty"`
+}
+
+type CreateProjectRequest struct {
+	Id                 primitive.ObjectID `bson:"_id"`
+	TeamId             *string            `bson:"teamId"`
+	ProjectName        *string            `json:"projectName"`
+	ProjectDescription *string            `json:"projectDescription"`
+	ProjectStatus      *string            `json:"projectStatus,omitempty"`
+	ProjectVideoLink   *string            `json:"projectVideoLink"`
+	ProjectGithubLink  *string            `json:"projectGithubLink"`
+	ProjectTrack       *string            `json:"projectTrack"`
+	ProjectTags        []string           `json:"projectTags"`
+	IsFinal            bool               `json:"isFinal,omitempty"`
+}
+
+type UpdateProjectRequest struct {
+	Id                 primitive.ObjectID `bson:"_id"`
+	TeamId             *string            `bson:"teamId"`
+	ProjectName        *string            `json:"projectName"`
+	ProjectDescription *string            `json:"projectDescription"`
+	ProjectStatus      *string            `json:"projectStatus,omitempty"`
+	ProjectVideoLink   *string            `json:"projectVideoLink"`
+	ProjectGithubLink  *string            `json:"projectGithubLink"`
+	ProjectTrack       *string            `json:"projectTrack"`
+	ProjectTags        []string           `json:"projectTags"`
+	IsFinal            bool               `json:"isFinal,omitempty"`
+}
