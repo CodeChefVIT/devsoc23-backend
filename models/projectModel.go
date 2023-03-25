d74ad12a3552176cf42589704187c14f256c9571
Akarsh Shukla <vyoopsoftwares@gmail.com>
2023-03-26 03:37:06 +0530

    Merge branch 'master' of https://github.com/akarsh1263/devsoc23-backend

--- models/projectModel.go
+++ models/projectModel.go
@@ -18,16 +18,12 @@ type Project struct {
 }
 
 type CreateProjectRequest struct {
-	Id                 primitive.ObjectID `bson:"_id"`
-	TeamId             *string            `bson:"teamId"`
-	ProjectName        *string            `json:"projectName"`
-	ProjectDescription *string            `json:"projectDescription"`
-	ProjectStatus      *string            `json:"projectStatus,omitempty"`
-	ProjectVideoLink   *string            `json:"projectVideoLink"`
-	ProjectGithubLink  *string            `json:"projectGithubLink"`
-	ProjectTrack       *string            `json:"projectTrack"`
-	ProjectTags        []string           `json:"projectTags"`
-	IsFinal            bool               `json:"isFinal,omitempty"`
+	ProjectName        *string  `json:"projectName"`
+	ProjectDescription *string  `json:"projectDescription"`
+	ProjectVideoLink   *string  `json:"projectVideoLink"`
+	ProjectGithubLink  *string  `json:"projectGithubLink"`
+	ProjectTrack       *string  `json:"projectTrack"`
+	ProjectTags        []string `json:"projectTags"`
 }
 
 type UpdateProjectRequest struct {
