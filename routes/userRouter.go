a567b6610363a5e49442bfe9023904f8167ba71b
Shivam Sharma <50650788+Mr-Emerald-Wolf@users.noreply.github.com>
2023-03-30 02:31:22 +0530

    feat: migrated refreshToken to redis from mongo (#35)

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -18,16 +18,15 @@ func UserRoutes(incomingRoutes *fiber.App, h *controller.Database) {
 	incomingRoutes.Post("/users/forgot/mail", h.ForgotPasswordMail)
 	incomingRoutes.Patch("/users/forgot", h.ForgotPassword)
 
-	
 	userGroup := incomingRoutes.Group("/users", middleware.VerifyToken)
 	userGroup.Get("/me", h.FindUser)
 	userGroup.Patch("/update", h.UpdateUser)
 	userGroup.Delete("/delete", h.DeleteUser)
 	userGroup.Get("/logout", h.LogoutUser)
 	userGroup.Get("/reset", h.ResetPassword)
-	
+	userGroup.Get("/me", h.FindUser)
+
 	adminGroup := incomingRoutes.Group("/admin", middleware.VerfiyAdmin)
-	adminGroup.Get("/me", h.FindUser)
 	adminGroup.Patch("/checkin", h.CheckIn)
 	adminGroup.Patch("/checkout", h.CheckOut)
 
