0879f4e53cf1f6cf93bb3df75c6994a79461ffb4
Jatin Manoj <jatin020403@gmail.com>
2023-04-21 22:13:12 +0530

    feat: remove teammate

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ import (
 func main() {
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		 AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-        AllowOrigins:     "*",
-        AllowCredentials: true,
-        AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
 	// Api rate limiter
