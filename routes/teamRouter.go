f4e92a39b948e05f5f62724aec2cfa919822a47a
Jatin Manoj <jatin020403@gmail.com>
2023-05-17 01:55:41 +0530

    fix: changed patch to post

--- routes/teamRouter.go
+++ routes/teamRouter.go
@@ -11,19 +11,19 @@ func TeamRoutes(incomingRoutes *fiber.App, h *controller.Database) {
 
 	teamGroup := incomingRoutes.Group("/team", middleware.VerifyToken)
 	teamGroup.Post("/create", h.CreateTeam)
-	teamGroup.Patch("/join/:teamId/:inviteCode", h.JoinTeam)
-	teamGroup.Patch("/leave", h.LeaveTeam)
+	teamGroup.Post("/join/:teamId/:inviteCode", h.JoinTeam)
+	teamGroup.Post("/leave", h.LeaveTeam)
 	teamGroup.Get("/get/:teamId", h.GetTeam)
 	teamGroup.Get("/all", h.GetTeams)
 	teamGroup.Get("/members/:teamId", h.GetTeamMembers)
 	teamGroup.Get("/ismember", h.GetIsMember)
-	teamGroup.Patch("/:teamId", h.UpdateTeam)
+	teamGroup.Post("/:teamId", h.UpdateTeam)
 	teamGroup.Delete("/:teamId", h.DeleteTeam)
-	teamGroup.Patch("/remove/:teamId/:memberId", h.RemoveMember)
+	teamGroup.Post("/remove/:teamId/:memberId", h.RemoveMember)
 
 	adminGroup := incomingRoutes.Group("/admin", middleware.VerfiyAdmin)
-	adminGroup.Patch("/promote/:teamId", h.PromoteTeam)
-	adminGroup.Patch("/finalise/:teamId", h.FinaliseTeam)
-	adminGroup.Patch("/disqualify/:teamId", h.DisqualifyTeam)
+	adminGroup.Post("/promote/:teamId", h.PromoteTeam)
+	adminGroup.Post("/finalise/:teamId", h.FinaliseTeam)
+	adminGroup.Post("/disqualify/:teamId", h.DisqualifyTeam)
 
 }
